3bd86e4227fab7c316c7a627dd5adb33dbc61c7c
Akim Krotov <akimsir@gmail.com>
2016-01-29 11:42:42 +0600

    Тесты

---
+++ unitTesting/Habbit.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace unitTesting
+{
+    public class Habbit
+    {
+        public void Add(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+
+        [Test]
+        public void HabbitValidationEmptyName()
+        {
+            Assert.Throws<ArgumentException>(()=>
+            {
+                var habbit = new Habbit();
+                habbit.Add("");
+            }
+        );
+        }
+        
+
+        //[Test]
+        //public void SeccundFailTest()
+        //{
+        //    Assert.AreSame("ololo", "olo1");
+        //}
+
+        //[Test]
+        //public void ParseCorrectString_ReturnInt()
+        //{
+        //    int result = int.Parse("56");
+
+        //    Assert.AreEqual(56, result);
+
+        //}
+
+        //[Test]
+        //public void Strings()
+        //{
+        //    bool result = "some str" == "some Str";
+
+        //    Assert.False(result);
+
+        //}
+
+        //[Test]
+        //public void IfStringIsNull()
+        //{
+        //    string nullStr = null;
+
+        //    Assert.Throws<NullReferenceException>(() => { nullStr.Length.ToString(); });
+        //}
+
+        public static int _random = 3;
+
+        [Test]
+        public void Repeating()
+        {
+            _random = new Random().Next(0, 60);
+
+            Assert.AreEqual(2, _random);
+        }
+
+        [Test]
+        public void Repeating2()
+        {
+            Assert.AreEqual(_random, DateTime.Now.Second);
+        }
+
+
+    }
+}
